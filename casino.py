a086a527fd1363c51cf97258433e4e3709e05a2b
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2018-08-10 04:39:19 +0200

    Add files via upload

---
+++ casino.py
@@ -0,0 +1,6 @@
+secret = 666
+guess = int(raw_input())
+if secret==guess:
+    print "Korrekt getippt"
+else:
+    print "Falsch getippt"
---
+++ non-english.py
@@ -0,0 +1,4 @@
+# coding=utf8
+string = "Köln"
+print string
+
